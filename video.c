6c18e4c747ce3da79fe224c04024b6b176a38080
bbm3h14aku <jan-frederic.kurzweil@bg.bib.de>
2016-05-04 09:27:27 +0200

    Update video.c

--- video.c
+++ video.c
@@ -16,8 +16,8 @@
 
 void kclrscr()
 {
-	ulong* vidmem = (ulong*) 0xb8000;
-	ulong i = 0;
+	unsigned long* vidmem = (unsigned long*) 0xb8000;
+	unsigned long i = 0;
 	while(i<(40*25))
 	{
 		vidmem[i] = 0x0A200A20;
@@ -25,7 +25,7 @@ void kclrscr()
 	};
 };
 
-unsigned int kprintf(char *message, uint line, char attribute)
+unsigned int kprintf(char *message, unsigned int line, char attribute)
 {
 	char* vidmem = (char*) 0xb8000;
 	uint i = line * 80 * 2;
@@ -56,8 +56,8 @@ void upcrs(int row, int col)
 	
 	//Cursor HIGH Port in vga Index Register
 	outportb(0x3D4, 0x0E);
-	outportb(0x3D5, (uchar) ((position>>8)&0xFF));
+	outportb(0x3D5, (unsigned char) ((position>>8)&0xFF));
 	// Cursor LOW Port in vga Index Register
 	outportb(0x3D4, 0x0F);
-	outportb(0x3D5, (ushort)(position & 0xFF));
+	outportb(0x3D5, (unsigned short)(position & 0xFF));
 };
